6bb9517fe143208fa6c1510cf0944b9a5aa77fcc
EC2 Default User <ec2-user@ip-172-31-66-179.ec2.internal>
2021-04-10 19:57:00 +0000

    added a form to app.js. The info in the form gets stored in a useState list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+  const [occupation, setoccupation] = useState(); //these are the react states that hold the form information
+  const [location, setlocation] = useState();
+  const [radius, setradius] = useState();
+  const [salary, setsalary] = useState();
+  const [ourform, setform] = useState([]);
+  const [submitting, setSubmitting] = useState(false); //this is the bool that gets used after the user clicks submit. it sends a message to user.
+  
+  
+  const handleSubmit = event => { //This function is called when the submit button is pressed. It creates a List from the form information and stores it in "ourform"
+    event.preventDefault();
+   setSubmitting(true);
+   var list=[];
+   list.push(occupation);
+   list.push(location);
+   list.push(radius);
+   list.push(salary);
+   setform(list);
+
+   setTimeout(() => {
+     setSubmitting(false);
+   }, 3000);
+ };
+
+  return(
+    <div className="mainform">
+      <h1>Job search</h1>
+      {submitting &&
+       <div>Searching jobs...</div>
+     }
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <div>Occupation: <input name="occupation" type='text' value={occupation} onChange={e => setoccupation(e.target.value)}/></div>
+            <div>Location: <input name="location" type='text' value={location} onChange={e => setlocation(e.target.value)}/></div>
+            <div>Radius: <input name="radius" type='text' value={radius} onChange={e => setradius(e.target.value)}/></div>
+            <div>Salary: <input name="salary" type='text' value={salary} onChange={e => setsalary(e.target.value)}/></div>
+          </label>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+      {ourform.map((objects) => (
+          objects
+        ))}
     </div>
-  );
+  )
 }
 
 export default App;
