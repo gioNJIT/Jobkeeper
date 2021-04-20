584dfc086927f41e1528054920597a33472b05b8
hb275 <78398284+hb275@users.noreply.github.com>
2021-04-19 21:58:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -54,9 +54,10 @@ def receiveParams(data):
     
     
     
-socketio.run(
-    app,
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-)
+if __name__ == "__main__":  
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+    )
 
