0a76a851553e39cb3b57d453f710b4011bced3fc
Giovanni DeRosa <ec2-user@ip-172-31-66-179.ec2.internal>
2021-04-28 21:52:52 +0000

    added models.py and verified that its working and receiving data to database

--- app.py
+++ app.py
@@ -6,11 +6,35 @@ from flask import Flask, send_from_directory
 import requests
 from Jooble_api import get_job_data
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__, static_folder='./build/static')
+
 
 load_dotenv(find_dotenv())
 
 api_key = os.getenv('api_key')
-BASE_URL = 'https://jooble.org/api/' + api_key
+BASE_URL = 'https://jooble.org/api/' + str(api_key)
+
+
+
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# IMPORTANT: This must be AFTER creating db variable to prevent
+# circular import issues
+from models import Person
+
+
+
+
+
+
 
 parameterList = []
 
@@ -59,10 +83,10 @@ def searchJob():
 #     print(job_details['titles'])
     
     
-    
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081,
-    debug=True,
-    use_reloader=True
-    )
+if __name__ == "__main__":    
+    app.run(
+        host=os.getenv('IP', '0.0.0.0'),
+        port=8081,
+        debug=True,
+        use_reloader=True
+        )
