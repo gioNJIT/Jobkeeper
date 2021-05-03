8cec016b10f027645f666cf96106af6200f0ed2e
hb275 <ec2-user@ip-172-31-17-183.ec2.internal>
2021-05-03 21:47:31 +0000

    pylint implemented and app.py is running

--- app.py
+++ app.py
@@ -1,167 +1,112 @@
-import functools
-import json
+'''
+This is the app.py
+'''
 import os
-from dotenv import load_dotenv,find_dotenv
-from flask import Flask, send_from_directory, request, jsonify, Response
-import requests
-from Jooble_api import get_job_data
-from flask_cors import CORS
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, send_from_directory, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from Jooble_api import get_job_data
 
 
-app = Flask(__name__, static_folder='./build/static')
+
+
+APP = Flask(__name__, static_folder='./build/static')
 
 
 load_dotenv(find_dotenv())
 
-api_key = os.getenv('api_key')
-BASE_URL = 'https://jooble.org/api/' + str(api_key)
+API_KEY = os.getenv('api_key')
+BASE_URL = 'https://jooble.org/api/' + str(API_KEY)
 
 
 
 # Point SQLAlchemy to your Heroku database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+APP.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 # Gets rid of a warning
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+DB = SQLAlchemy(APP)
 
 # IMPORTANT: This must be AFTER creating db variable to prevent
 # circular import issues
 import models  # pylint: disable=wrong-import-position
 
 
+APP = Flask(__name__, static_folder='./build/static')
+APP.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
+CORS = CORS(APP, resources={r"/*": {"origins": "*"}})
 
 
-
-
-app = Flask(__name__, static_folder='./build/static')
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
-
-@app.route('/', defaults={"filename": "index.html"})
-@app.route('/<path:filename>')
+@APP.route('/', defaults={"filename": "index.html"})
+@APP.route('/<path:filename>')
 def index(filename):
     '''
     This is a index function
     '''
     return send_from_directory('./build', filename)
 
-@app.route('/api/v1/job/userInfo', methods=['POST'])
-def userInfo():
+@APP.route('/api/v1/job/userInfo', methods=['POST'])
+def user_info():
+    '''
+    This is the user information function
+    '''
     print(request.get_json())
     return "Test"
-    
-    
-@app.route('/api/v1/job/searchJob', methods=['GET'])
-def searchJob():
-    parameterList = []
+@APP.route('/api/v1/job/searchJob', methods=['GET'])
+def search_job():
+    '''
+    This is a job search function
+    '''
+    parameter_list = []
     if 'occupation' in request.args:
-        parameterList.append(request.args['occupation'])
-        parameterList.append(request.args['location'])
-        parameterList.append(request.args['radius'])
-        parameterList.append(request.args['salary'])
-        
-        job_details = get_job_data(parameterList)
+        parameter_list.append(request.args['occupation'])
+        parameter_list.append(request.args['location'])
+        parameter_list.append(request.args['radius'])
+        parameter_list.append(request.args['salary'])
+        job_details = get_job_data(parameter_list)
         print(job_details)
         alljob_dict = {}
-        if(job_details['total_jobs'] <= 5):
+        if job_details['total_jobs'] <= 5:
             total = job_details['total_jobs']
         else:
             total = 5
-            
-        
         for job in range(0, total):
-            alljob_dict.update({job: [job_details['titles'][job], job_details['locations'][job], job_details['salaries'][job], job_details['ids'][job]]})
-        #alljob_dict = {0:[job_details['titles'][0], job_details['locations'][0], job_details['salaries'][0], job_details['ids'][0]]}
-        title_arr = job_details['titles'][0]
+            alljob_dict.update({job:[job_details['titles'][job], job_details['locations'][job], job_details['salaries'][job], job_details['ids'][job]]})
+        #title_arr = job_details['titles'][0]
         print(alljob_dict)
         return jsonify(alljob_dict)
-        
-        
-    else:
-        print("Could not find it")
-        return "Something went wrong"
-        
-    #print("Params received")
-
-
-@app.route('/api/v1/job/getfavJob', methods=['GET'])
-def getfavJob():
-    favjobDict={}
+    return "Something went wrong"
+@APP.route('/api/v1/job/getfav_job', methods=['GET'])
+def getfav_job():
+    '''
+    This is the favorite function
+    '''
+    favjob_dict = {}
     if 'id' in request.args:
         player = models.Person.query.filter_by(id=request.args["id"]).first()
-        
         print("received id:")
         print(id)
         print("favorites list from id:")
         print(player.favorites)
-        
-        for x in range(len(player.favorites)):
-            
-            jobquery= models.Jobs.query.filter_by(job_id=player.favorites[x]).first()
-            favjobDict[x]=[jobquery.job_id,jobquery.job_title,jobquery.job_location,jobquery.job_salary]
-            print(favjobDict[x])
-        
-        
-        return jsonify(favjobDict)
-        
-        
-    else:
-        print("Could not find it")
-        return "Something went wrong"
-
-
-
-    
-@app.route('/api/v1/job/Favorites', methods=['GET'])
+        for fav in range(len(player.favorites)):
+            jobquery = models.Jobs.query.filter_by(job_id=player.favorites[fav]).first()
+            favjob_dict[fav] = [jobquery.job_id, jobquery.job_title, jobquery.job_location, jobquery.job_salary]
+            print(favjob_dict[fav])
+        return jsonify(favjob_dict)
+    return "Something went wrong"
+@APP.route('/api/v1/job/Favorites', methods=['GET'])
 def add_favourites():
+    '''
+    This is the add to favorites function
+    '''
     data = request.args['favorite'].split(',')
     print(data[0])
     return "Something went wrong"
-
-
-
-"""EXAMPLE FUNCTION TO BE USED TO RETREIVE DATA FROM DATABASE"""    
-"""def add_users():
-    player = models.Person.query.filter_by(id=123321).first()
-    if player is None:
-        print("could not find id number")
-        #addplayer = models.Person(username=data['user'], score=100)
-        #DB.session.add(addplayer)  # pylint: disable=no-member
-        #DB.session.commit()  # pylint: disable=no-member
-    else:
-        print("player")
-        print(player.id)
-    
-    
-# @socketio.on('UserLoggedIn')
-# def on_UserLoggedIn():
-#     socketio.emit('UserLoggedIn', broadcast=True, include_self=True)
-#     print("user has logged in")
-    
-    
-add_users() """
-
-    
-    
-    
-# @socketio.on('sendParams')
-# def receiveParams(data):
-#     parameterList = data["userParams"]
-#     job_details = get_job_data(parameterList)
-#     title_arr = job_details['titles']
-#     socketio.emit( "Updated_details",title_arr, broadcast=True, include_self=True)
-#     print(job_details['titles'])
-    
-    
-if __name__ == "__main__":    
-    app.run(
+if __name__ == "__main__":
+    APP.run(
         host=os.getenv('IP', '0.0.0.0'),
         port=8081,
         debug=True,
